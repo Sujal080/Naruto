723a9f4f962a60f5dc2a24fb3c987640b4e74d21
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-06-27 16:43:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
+    BOT_TOKEN = "8087125389:AAFQm9W73dxk2H0rzZe87CSMQRijGbPfmU8"
     
-    API_ID = 12475131
+    API_ID = 25933223
     
-    API_HASH = "719171e38be5a1f500613837b79c536f"
-    ADMIN_ID = [12475131]
+    API_HASH = "6ef5a426d85b7f01562a41e6416791d3"
+    ADMIN_ID = [8118667253]
     
-    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://biklriplit:efaXfv2Ps9MRfner@cluster0.4hfu8zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002542634912
+    TXT_LOG = -1002743806015
     
-    AUTH_LOG = -1002542634912
+    AUTH_LOG = -1002743806015
     
-    HIT_LOG = -1002542634912
+    HIT_LOG = -1002743806015
     
-    DRM_DUMP = -1002542634912
+    DRM_DUMP = -1002743806015
     # Main channel ki ID
-    CHANNEL = -1002542634912
+    CHANNEL = -1002743806015
     # Channel ka link
-    CH_URL = "https://t.me/gjskisb"
+    CH_URL = "https://t.me/+aHEKrxF0el4yYzk1"
     
-    OWNER = "https://t.me/gjskisb"
+    OWNER = "https://t.me/+aHEKrxF0el4yYzk1"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
